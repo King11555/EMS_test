35b05646d5a9948a3e20f57d3175e08921b0f5e4
King11555 <kralj.filip007@gmail.com>
2026-03-16 12:04:24 +0100

    Add files via upload

--- update.sh
+++ update.sh
@@ -6,20 +6,14 @@ LOCAL_FOLDER="./"
 # Make sure folder exists
 mkdir -p "$LOCAL_FOLDER"
 
-# Update EMS_test.py
+# Download EMS_test.py
 echo "Updating EMS_test.py ..."
-wget -q -O "$LOCAL_FOLDER/EMS_test.py" "https://raw.githubusercontent.com/King11555/EMS_test/refs/heads/main/EMS_test.py"
-if [ $? -eq 0 ]; then
-    echo "✅ EMS_test.py updated successfully."
-else
-    echo "❌ Failed to update EMS_test.py."
-fi
+wget -q -O "$LOCAL_FOLDER/EMS_test.py" "https://raw.githubusercontent.com/King11555/EMS_test/refs/heads/main/EMS_test.py" \
+    && echo "✅ EMS_test.py updated successfully." \
+    || echo "❌ Failed to update EMS_test.py."
 
-# Update config.yaml
+# Download config.yaml
 echo "Updating config.yaml ..."
-wget -q -O "$LOCAL_FOLDER/config.yaml" "https://raw.githubusercontent.com/King11555/EMS_test/refs/heads/main/config.yaml"
-if [ $? -eq 0 ]; then
-    echo "✅ config.yaml updated successfully."
-else
-    echo "❌ Failed to update config.yaml."
-fi
+wget -q -O "$LOCAL_FOLDER/config.yaml" "https://raw.githubusercontent.com/King11555/EMS_test/refs/heads/main/config.yaml" \
+    && echo "✅ config.yaml updated successfully." \
+    || echo "❌ Failed to update config.yaml."
